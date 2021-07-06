46ac0231b8dae1fd275cfe6736636db9c3cb2b44
kalindia <85122243+kalindia@users.noreply.github.com>
2021-07-06 17:07:37 +0530

    Add files via upload

---
+++ duck hunt game/div se color ka use/script.js
@@ -0,0 +1,109 @@
+
+
+
+function demo()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="red";
+}
+
+function ram()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="green";
+}
+
+
+
+function kiran()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="blue";
+}
+
+function jaya()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="blue";
+}
+
+
+
+function tanvi()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="orange";
+}
+
+
+function ravi()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="yellow";
+}
+
+
+function rajni()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="pink";
+}
+
+
+function pooja()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="purple";
+}
+
+
+function ganpti()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="skyblue";
+}
+
+
+
+
+
+function shriram()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="darkred";
+}
+
+
+
+function wap()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="darkorange";
+}
+
+
+function mohan()
+
+{
+	var x = document.getElementById("result");
+	x.style.color ="darkblue";
+}
+
+
+
+
+
+
+
+
